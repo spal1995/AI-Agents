06e24fab3d0cc37aaeec06aa712074c74448e12e
Shubhrajit Pal <70978635+spal1995@users.noreply.github.com>
2025-04-22 20:54:16 +0200

    Add files via upload

---
+++ Phidata_agents/LLM_Assitant/installcheck.py
@@ -0,0 +1,6 @@
+import typer
+from typing import Optional, List
+from phi.assistant import Assistant
+from phi.storage.assistant.postgres import PgAssistantStorage
+from phi.knowledge.pdf import PDFUrlKnowledgeBase
+from phi.vectordb.pgvector import PgVector2
